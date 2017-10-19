3415df0908f0d90e54e825785d40346ff2386338
Tim <tim@vapor.io>
2017-10-19 14:08:37 -0700

    Gofmt run

--- cmd/ksync/delete_test.go
+++ cmd/ksync/delete_test.go
@@ -1,21 +1,20 @@
 package main
 
 import (
-  "testing"
-  "reflect"
+	"reflect"
+	"testing"
 
-  "github.com/stretchr/testify/assert"
-  // "github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
+	// "github.com/stretchr/testify/require"
 
-  "github.com/spf13/cobra"
-  // "github.com/spf13/viper"
-
+	"github.com/spf13/cobra"
+	// "github.com/spf13/viper"
 )
 
 func TestDeleteNew(t *testing.T) {
-  testCobra := &deleteCmd{}
-  cmd := testCobra.new()
+	testCobra := &deleteCmd{}
+	cmd := testCobra.new()
 
-  assert.IsTypef(t, reflect.TypeOf(&cobra.Command{}), reflect.TypeOf(cmd), "New command is of type %s", reflect.TypeOf(cmd))
-  // TODO: Write more specific test cases
+	assert.IsTypef(t, reflect.TypeOf(&cobra.Command{}), reflect.TypeOf(cmd), "New command is of type %s", reflect.TypeOf(cmd))
+	// TODO: Write more specific test cases
 }
--- cmd/ksync/list_test.go
+++ cmd/ksync/list_test.go
@@ -1,21 +1,20 @@
 package main
 
 import (
-  "testing"
-  "reflect"
+	"reflect"
+	"testing"
 
-  "github.com/stretchr/testify/assert"
-  // "github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
+	// "github.com/stretchr/testify/require"
 
-  "github.com/spf13/cobra"
-  // "github.com/spf13/viper"
-
+	"github.com/spf13/cobra"
+	// "github.com/spf13/viper"
 )
 
 func TestListNew(t *testing.T) {
-  testCobra := &listCmd{}
-  cmd := testCobra.new()
+	testCobra := &listCmd{}
+	cmd := testCobra.new()
 
-  assert.IsTypef(t, reflect.TypeOf(&cobra.Command{}), reflect.TypeOf(cmd), "New command is of type %s", reflect.TypeOf(cmd))
-  // TODO: Write more specific test cases
+	assert.IsTypef(t, reflect.TypeOf(&cobra.Command{}), reflect.TypeOf(cmd), "New command is of type %s", reflect.TypeOf(cmd))
+	// TODO: Write more specific test cases
 }
