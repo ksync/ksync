5d68df14d179b079425578a0750da7e3d4c06b23
Tim <tim@vapor.io>
2017-10-23 13:15:40 -0700

    Integration testing first pass

---
+++ pkg/cli/config_test.go
@@ -0,0 +1,15 @@
+package cli
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+)
+
+func TestInitConfig(t *testing.T) {
+	configname := "ksync"
+	err := InitConfig(configname)
+
+	require.NoError(t, err)
+}
