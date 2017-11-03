f7656e475ebd198132725f7d733a438ec0f3e71e
Tim <tim@vapor.io>
2017-11-03 13:27:58 -0700

    Partial test update

--- pkg/ksync/container_test.go
+++ pkg/ksync/container_test.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-
 )
 
 func init() {
@@ -14,17 +13,17 @@ func init() {
 
 func TestRadar(t *testing.T) {
 	con := &Container{
-		ID: "",
+		ID:   "",
 		Name: "",
-    // TODO: This has to be dynamic
-		NodeName: "gke-tim-dev-default-pool-8e1c25a0-j3kp",
-		PodName: "",
+		// TODO: This has to be dynamic
+		NodeName: "gke-tim-dev-default-pool-9e45a876-pzbw",
+		PodName:  "",
 	}
 	_, err := con.Radar()
-  // TODO: Remove logging
+	// TODO: Remove logging
 	t.Log(err)
 
-  // TODO: Is there anything else we can test? There has to be a better way of doing this.
+	// TODO: Is there anything else we can test? There has to be a better way of doing this.
 	require.NoError(t, err)
 }
 
@@ -33,11 +32,11 @@ func TestGetByName(t *testing.T) {
 	podName := "ksync-radar-01q6r"
 	containerName := "someequallystupidname"
 
-  // Test erroring on empty containerName
+	// Test erroring on empty containerName
 	_, err := GetByName(podName, "")
 	assert.NoError(t, err)
 
-  // Test default error
+	// Test default error
 	_, err = GetByName(podName, containerName)
 	require.Error(t, err)
 }
