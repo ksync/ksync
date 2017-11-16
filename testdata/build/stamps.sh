25c080bfe91c85b5ff375f0229f35807cbb38306
Tim <tim@vapor.io>
2017-11-16 15:44:01 -0800

    *Now* it wants the full path

--- testdata/build/stamps.sh
+++ testdata/build/stamps.sh
@@ -14,17 +14,16 @@ BUILD_DATE=${BUILD_DATE:-$(date --utc --rfc-3339 ns 2> /dev/null | sed -e 's/ /T
 GO_VERSION=${GO_VERSION:-$(go version | awk '{ print $3 }')}
 
 # Setup ldflags for runs
-# THIS MUST BE RUN FROM THE ROOT REPO DIR!
 export LDFLAGS="\
     -w \
-    -X ksync.GitCommit=${GIT_COMMIT} \
-    -X ksync.BuildDate=${BUILD_DATE} \
-    -X ksync.VersionString=${BINARY_VERSION} \
-    -X ksync.GoVersion=${GO_VERSION} \
-    -X radar.GitCommit=${GIT_COMMIT} \
-    -X radar.BuildDate=${BUILD_DATE} \
-    -X radar.VersionString=${BINARY_VERSION} \
-    -X radar.GoVersion=${GO_VERSION} \
+    -X github.com/vapor-ware/ksync/pkg/ksync.GitCommit=${GIT_COMMIT} \
+    -X github.com/vapor-ware/ksync/pkg/ksync.BuildDate=${BUILD_DATE} \
+    -X github.com/vapor-ware/ksync/pkg/ksync.VersionString=${BINARY_VERSION} \
+    -X github.com/vapor-ware/ksync/pkg/ksync.GoVersion=${GO_VERSION} \
+    -X github.com/vapor-ware/ksync/pkg/radar.GitCommit=${GIT_COMMIT} \
+    -X github.com/vapor-ware/ksync/pkg/radar.BuildDate=${BUILD_DATE} \
+    -X github.com/vapor-ware/ksync/pkg/radar.VersionString=${BINARY_VERSION} \
+    -X github.com/vapor-ware/ksync/pkg/radar.GoVersion=${GO_VERSION} \
     ${LDFLAGS:-} \
 "
 
