4e48dc6fe5c3ab17b9f7034a3879f0dab9cc7fb0
Tim <tim@vapor.io>
2017-11-20 11:29:54 -0800

    Added test for FetchMirror()

--- pkg/ksync/fetch_test.go
+++ pkg/ksync/fetch_test.go
@@ -1,1 +1,14 @@
 package ksync
+
+import (
+  "testing"
+
+  "github.com/stretchr/testify/assert"
+  // "github.com/stretchr/testify/require"
+)
+
+func TestFetchMirror(t *testing.T) {
+  err := FetchMirror()
+
+  assert.NoError(t, err)
+}
