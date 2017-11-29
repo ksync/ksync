b0c4e7ae3dad5543e1f5960ef6e5fe4f97ceed17
Tim <tim@vapor.io>
2017-11-29 15:21:48 -0800

    Quick godoc formatting updates.

--- pkg/input/doc.go
+++ pkg/input/doc.go
@@ -2,7 +2,7 @@
 Package input helps consume and validate user input.
 
 This includes:
-- A container for paths that are defined as a set of sources and targets.
-- The set of options required to locate a specific container remotely.
+  - A container for paths that are defined as a set of sources and targets.
+  - The set of options required to locate a specific container remotely.
 */
 package input
