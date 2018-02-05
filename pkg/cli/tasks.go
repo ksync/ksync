1bfac5055f5721a96d7f277ba9cf4540e8c80c83
Thomas Rampelberg <thomas@saunter.org>
2018-02-05 13:35:22 -0600

    Fixes up some of the log issues around spinners.
    - Fixes #132

--- pkg/cli/tasks.go
+++ pkg/cli/tasks.go
@@ -2,22 +2,30 @@ package cli
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/briandowns/spinner"
 	"github.com/logrusorgru/aurora"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 // TaskOut provides a way to pretty print a long running function with
 // colors and spinners.
 func TaskOut(name string, fn func() error) error {
+	isTerm := terminal.IsTerminal(int(os.Stdout.Fd()))
+
 	spin := spinner.New(spinner.CharSets[11], 100*time.Millisecond)
 	spin.Prefix = fmt.Sprintf("%-40s    ", name)
-	if err := spin.Color("yellow"); err != nil {
-		return err
-	}
 
-	spin.Start()
+	if isTerm && log.GetLevel() <= log.InfoLevel {
+		// Color restarts the spinner, so it needs to be in here.
+		if err := spin.Color("yellow"); err != nil {
+			return err
+		}
+		spin.Start()
+	}
 
 	err := fn()
 
@@ -27,7 +35,12 @@ func TaskOut(name string, fn func() error) error {
 	}
 
 	spin.FinalMSG = fmt.Sprintf("%-40s    %s\n", name, result)
-	spin.Stop()
+
+	if isTerm && log.GetLevel() <= log.InfoLevel {
+		spin.Stop()
+	} else {
+		fmt.Printf(spin.FinalMSG)
+	}
 
 	if err != nil {
 		fmt.Printf("%s\t%s\n", aurora.Red("\u21b3"), err)
