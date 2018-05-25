d720170c197ffb276b7d5f0b5aa632660be10a6c
Tim <tim@vapor.io>
2018-05-24 17:05:11 -0700

    Implement `delete --all`
    Signed-off-by: Tim <tim@vapor.io>

--- cmd/ksync/delete.go
+++ cmd/ksync/delete.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/empty"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 
 	"github.com/vapor-ware/ksync/pkg/cli"
 	"github.com/vapor-ware/ksync/pkg/ksync"
+	pb "github.com/vapor-ware/ksync/pkg/proto"
 )
 
 type deleteCmd struct {
@@ -29,26 +37,77 @@ func (d *deleteCmd) new() *cobra.Command {
 		Run:     d.run,
 	})
 
+	flags := d.Cmd.Flags()
+
+	flags.Bool(
+		"all",
+		false,
+		"delete all specs")
+	if err := d.BindFlag("all"); err != nil {
+		log.Fatal(err)
+	}
+
 	return d.Cmd
 }
 
 func (d *deleteCmd) run(cmd *cobra.Command, args []string) {
-	for name := range args {
-		specs := &ksync.SpecList{}
-		if err := specs.Update(); err != nil {
-			log.Fatal(err)
+	if viper.GetBool("all") {
+		d.deleteAll()
+	} else {
+		for name := range args {
+			d.delete(args[name])
 		}
+	}
+}
 
-		if !specs.Has(args[name]) {
-			log.Fatalf("%s does not exist. Did you mean something else?", args[name])
-		}
+func (d *deleteCmd) delete(name string) {
+	specs := &ksync.SpecList{}
+	if err := specs.Update(); err != nil {
+		log.Fatal(err)
+	}
 
-		if err := specs.Delete(args[name]); err != nil {
-			log.Fatalf("Could not delete %s: %v", args[name], err)
-		}
+	if !specs.Has(name) {
+		log.Fatalf("%s does not exist. Did you mean something else?", name)
+	}
 
-		if err := specs.Save(); err != nil {
-			log.Fatal(err)
-		}
+	if err := specs.Delete(name); err != nil {
+		log.Fatalf("Could not delete %s: %v", name, err)
+	}
+
+	if err := specs.Save(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (d *deleteCmd) deleteAll() {
+	// This is connecting locally and it is very unlikely watch is overloaded,
+	// set the timeout *super* short to make it easier on the users when they
+	// forgot to start watch.
+	withTimeout, _ := context.WithTimeout(context.TODO(), 100*time.Millisecond)
+
+	conn, err := grpc.DialContext(
+		withTimeout,
+		fmt.Sprintf("127.0.0.1:%d", viper.GetInt("port")),
+		[]grpc.DialOption{
+			grpc.WithBlock(),
+			grpc.WithInsecure(),
+		}...)
+	if err != nil {
+		// The assumption is that the only real error here is because watch isn't
+		// running
+		log.Debug(err)
+		log.Fatal("Having problems querying status. Are you running watch?")
+	}
+	defer conn.Close() // nolint: errcheck
+
+	client := pb.NewKsyncClient(conn)
+
+	resp, err := client.GetSpecList(context.Background(), &empty.Empty{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for name := range resp.Items {
+		d.delete(fmt.Sprintf("%s", name))
 	}
 }
