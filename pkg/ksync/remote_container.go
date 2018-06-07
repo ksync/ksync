46ad6301437ccdad344e1baab8f9a129d033b175
Tim <timfallmk@users.noreply.github.com>
2018-06-07 11:34:06 -0700

    Formating run (#191)

--- pkg/ksync/remote_container.go
+++ pkg/ksync/remote_container.go
@@ -92,10 +92,10 @@ func (c *RemoteContainer) Message() (*pb.RemoteContainer, error) {
 // DeserializeRemoteContainer deserializes gRPC messages into a RemoteContainer struct
 func DeserializeRemoteContainer(c *pb.RemoteContainer) (*RemoteContainer, error) {
 	result := &RemoteContainer{
-		ID: c.GetId(),
-		Name: c.GetContainerName(),
+		ID:       c.GetId(),
+		Name:     c.GetContainerName(),
 		NodeName: c.NodeName,
-		PodName: c.GetPodName(),
+		PodName:  c.GetPodName(),
 	}
 	return result, nil
 }
